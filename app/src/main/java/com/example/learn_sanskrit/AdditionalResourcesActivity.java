ff6fb8dab439fa603deb08babe982b4596899a82
Jayant Kulkarni <jayantkulkarni999@gmail.com>
2020-12-25 09:56:43 +0530

    Created style for category and content. Other activities are yet to be given intent from main activity.

---
+++ app/src/main/java/com/example/learn_sanskrit/AdditionalResourcesActivity.java
@@ -0,0 +1,13 @@
+package com.example.learn_sanskrit;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AdditionalResourcesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_additional_resources);
+    }
+}
